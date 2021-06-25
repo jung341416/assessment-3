f1f564398cc6cca88cd464c76a88e1fc76008cd9
jung341416 <jung341416@gmail.com>
2021-06-25 17:02:34 -0400

    second portion

---
+++ website/index.js
@@ -0,0 +1,14 @@
+//grab DOM elements
+const shopping_btn = document.getElementById('shopping_cart');
+const add_btn = document.getElementById('click_btn');
+const item_name = document.getElementsByClassName('image_name');
+const item_price = document.getElementsByClassName('image_price');
+const display_name = document.getElementsByClassName('display_name');
+const display_price = document.getElementsByClassName('display_price');
+//when buttons clicked set the item and price it to local storage
+// when shoppingcart is clicked add a href which will locate to new page
+  //display the stored values from localstorage on new page
+
+  windows.onload((event)=> {
+    event.target = 
+  })
