c15470452603912bb7e863ae6d8c402f0256f83e
jung341416 <jung341416@gmail.com>
2021-06-25 12:45:26 -0400

    initial commit for schema design

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,39 @@
 -- Schema creation goes here
+--pseudo
+  --customer can have many products
+  --each product can belong to only one customer
+  --product belong to only one category
+  --category can have many products
+  --order information contain many product and category
+DROP TABLE customers IF EXISTS CASCADE;
+DROP TABLE products IF EXISTS CASCADE;
+DROP TABLE categories IF EXISTS CASCADE;
+DROP TABLE order_informations IF EXISTS CASCADE;
+
+CREATE TABLE customers (
+  id
+);
+customers
+id
+first_name
+last_name
+email_address
+
+products
+id
+name
+price
+customer_id fk customers(id)
+
+categories
+id
+name
+product_id fk products(id)
+
+
+
+order_informations
+id
+order_date
+categories_id
+product_id
